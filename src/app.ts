a3469d07287f636fb2e1c057468505759e2bb9f3
Carlsson Khovis <carlkho99@gmail.com>
2026-05-04 00:05:00 +0700

    ionkno man

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express on Vercel!' });
+});
+
 app.use("/user", userRoutes);
 app.use("/tournament", tournamentRoutes);
 app.use("/organization", organizationRoutes);
