1464292d85a0a920aa714fa47c33e266ec9ca97c
Carlsson Khovis <carlkho99@gmail.com>
2026-05-03 23:54:24 +0700

    fixed imports fr

--- app/routes/tournament.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import TournamentController from "../controllers/tournament";
-
-const routes = Router();
-
-routes.get("/", TournamentController.getAll);
-
-export default routes;
